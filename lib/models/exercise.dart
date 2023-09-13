a1d80ebbe9e2c665a2fb90ac733d412fd6c77f12
Abdulmalik <omayri970@gmail.com>
2023-09-14 00:21:27 +0300

    init

---
+++ lib/models/exercise.dart
@@ -0,0 +1,12 @@
+class Exercise {
+  final String name, weight, reps, sets;
+  bool isCompleted;
+
+  Exercise({
+    required this.name,
+    required this.weight,
+    required this.reps,
+    required this.sets,
+    this.isCompleted = false,
+  });
+}
