cb0749c1a283188b93ce402851256d8da2dd8702
Abdulmalik <omayri970@gmail.com>
2023-09-15 18:46:02 +0300

    add gender options

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,73 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
-  
+enum Gender {
+  male,
+  female,
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() {
+  runApp(
+    MaterialApp(home: InputScreen()), // use MaterialApp
+  );
+}
+
+class InputScreen extends StatefulWidget {
+  @override
+  _InputScreenState createState() => _InputScreenState();
+}
+
+class _InputScreenState extends State<InputScreen> {
+  Color inactiveColor = Color(0xFF24232F);
+  Color activeColor = Colors.blueGrey;
+  late Gender selectedGender;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("BMI"),
+        ),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              GestureDetector(
+                onTap: (){
+
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: selectedGender == Gender.male?activeColor:inactiveColor
+                  ),
+                child: Column(
+                  children: [
+                    Icon(
+                      FontAwesomeIcons.person,
+                      color: Colors.white,
+                      size: 45.0
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "male",
+                      style: (TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500,
+                      )),
+                    )
+                  ],
+                ),
+                ),
+              ),
+            ]
+          )
+        ]
+      )
     );
   }
 }
-
