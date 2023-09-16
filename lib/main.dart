4ed83a5d9415eabb85a316f9c4fcd1aaf1b60efe
Abdulmalik <omayri970@gmail.com>
2023-09-16 23:12:42 +0300

    counter game

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,19 @@ void main() {
   return runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+// Immutable widget 
+class MyApp extends StatefulWidget {
   const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+// Mutable widget
+class _MyAppState extends State<MyApp> {
+
+  int counter = 0;
+  var mode = ["Easy", "Medium", "Hard", "Impossible"];
 
   @override
   Widget build(BuildContext context) {
@@ -17,22 +28,41 @@ class MyApp extends StatelessWidget {
         title: Center(
             child: const Text("أهلا بالعالم")),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.end,
-
-        children: [
-          Icon(Icons.backpack),
-          Icon(Icons.leaderboard),
-          Icon(Icons.person),
-
-        ]
-
-
+      
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          setState(() {
+            counter++;
+          });
+        },
       ),
-
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text((() {
+              if(counter < 5){
+                return mode[0];
+              } else if(counter < 10){
+                return mode[1];
+              } else if(counter < 15){
+                return mode[2];
+              } else {
+                return mode[3];
+              }
+            })()),
+            Text(
+          "$counter",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.black54,
+          ),),
+          ],),
+          
+      ),
     ),
   );
   }
-
 }
