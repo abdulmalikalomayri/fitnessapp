8f10dfc24e7f49d0d74b6f99995d427467fd3f67
Malek <omayri970@gmail.com>
2023-09-21 15:24:30 +0300

    create about page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:myflutterapp/view/about.dart';
 
 enum Gender {
   male,
@@ -8,66 +9,41 @@ enum Gender {
 
 void main() {
   runApp(
-    MaterialApp(home: InputScreen()), // use MaterialApp
+    MaterialApp(home: MyApp()), // use MaterialApp
   );
 }
 
-class InputScreen extends StatefulWidget {
+class MyApp extends StatefulWidget {
   @override
-  _InputScreenState createState() => _InputScreenState();
+  _MyAppState createState() => _MyAppState();
+
 }
 
-class _InputScreenState extends State<InputScreen> {
-  Color inactiveColor = Color(0xFF24232F);
-  Color activeColor = Colors.blueGrey;
-  late Gender selectedGender;
-
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text("BMI"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Home Page"),
         ),
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              GestureDetector(
-                onTap: (){
-
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: selectedGender == Gender.male?activeColor:inactiveColor
+        body: Center(
+          child: ElevatedButton(
+            child: Text("Go to About Page"),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => About(),
                   ),
-                child: Column(
-                  children: [
-                    Icon(
-                      FontAwesomeIcons.person,
-                      color: Colors.white,
-                      size: 45.0
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(
-                      "male",
-                      style: (TextStyle(
-                        color: Colors.grey,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w500,
-                      )),
-                    )
-                  ],
-                ),
-                ),
-              ),
-            ]
+              );
+            },
           )
-        ]
-      )
+        )
+        )
     );
   }
 }
+
+
+
---
+++ lib/view/about.dart
@@ -0,0 +1,18 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/material.dart';
+
+class About extends StatelessWidget {
+  const About({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar:AppBar(
+        title: Text("About"),
+        backgroundColor: Colors.green,
+      ),
+      
+    );
+  }
+}
